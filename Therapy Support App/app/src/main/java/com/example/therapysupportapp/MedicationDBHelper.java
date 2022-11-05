697deba627dfcc0e5afc529321a1e59cba38a2cf
Michael <44344017+mtangtran@users.noreply.github.com>
2022-11-05 16:18:01 -0400

    Initial creation of MedicationDBHelper.java. This class will support medication for now. Base feature of creating and adding the database has been established.

---
+++ Therapy Support App/app/src/main/java/com/example/therapysupportapp/MedicationDBHelper.java
@@ -0,0 +1,77 @@
+package com.example.therapysupportapp;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+/**
+ *
+ * Medication class.
+ *
+ * The code references two resources:
+ * 1. https://www.geeksforgeeks.org/how-to-create-and-add-data-to-sqlite-database-in-android/
+ * 2. https://www.youtube.com/watch?v=aQAIMY-HzL8
+ * */
+
+
+public class MedicationDBHelper extends SQLiteOpenHelper {
+    private static final String DB_Name = "Medication";
+    private static final int DB_version = 1;
+
+    private static final String Table_Name = "medications";
+    private static final String ID_COL  = "id";
+    private static final String medicationName_COL = "MED_NAME";
+    private static final String dosage_COL = "dosage";
+    private static final String dosageQuantity_COL = "dosage_quantity";
+    private static final String inventory_COL = "inventory";
+    private static final String inventoryReminder_COL = "inventory_reminder";
+    private static final String history_COL = "history";
+
+    public MedicationDBHelper(@Nullable Context context) {
+        super(context, DB_Name, null, DB_version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String query = "CREATE TABLE " + Table_Name + " ("
+                + ID_COL + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + medicationName_COL + " TEXT,"
+                + dosage_COL + " TEXT,"
+                + dosageQuantity_COL + " TEXT,"
+                + inventory_COL + " Text,"
+                + inventoryReminder_COL + " TEXT,"
+                + history_COL + " TEXT)";
+
+        // at last we are calling a exec sql
+        // method to execute above sql query
+        sqLiteDatabase.execSQL(query);
+    }
+
+    public void addNewMedication(String id, String medName, String dosage, String dosageQuantity, String inventory, String inventoryReminder, String history){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        // Create the values
+        values.put(ID_COL, id);
+        values.put(medicationName_COL, medName);
+        values.put(dosage_COL, dosage);
+        values.put(dosageQuantity_COL, dosageQuantity);
+        values.put(inventory_COL, inventory);
+        values.put(inventoryReminder_COL, inventoryReminder);
+        values.put(history_COL, history);
+
+        // Store the values into the database table.
+        db.insert(Table_Name, null, values);
+
+        db.close();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // this method is called to check if the table exists already.
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + Table_Name);
+        onCreate(sqLiteDatabase);
+    }
+}
